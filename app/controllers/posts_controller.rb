279df4f27c9b7a77158b568d3172aa9b893acfb2
tclewis91 <tclewis91@gmail.com>
2015-06-09 15:54:36 -0500

    crud methods added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
-end
+
+  def index
+    all_posts = Post.all
+    respond_to do |format|
+      format.html do
+        render 'index.html.erb', locals {posts: all_posts}
+      end
+    end
+  end
+
+  def show
+    search_blog = Post.find(params[:id])
+    respond to do |format|
+      format.html do
+        render 'show.html.erb', locals: {posts_serach: search_posts}
+      end
+    end
+  end
+
+  def destroy
+    if Post.exists?(params[:id])
+      Post.destroy(params[:id])
+    render 'destroy.html.erb' #check to change
