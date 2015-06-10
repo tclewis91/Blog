29bf463a50fad9d2f57f8fb3016790f56afe881e
tclewis91 <tclewis91@gmail.com>
2015-06-10 06:57:07 -0500

    security add on

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,23 @@ class PostsController < ApplicationController
     all_posts = Post.all
     respond_to do |format|
       format.html do
-        render 'index.html.erb', locals {posts: all_posts}
+        render 'index.html.erb', locals: {posts: all_posts}
+      end
+    end
+  end
+
+
+  def new
+    render 'new.html.erb', locals: { post: Post.new}
+  end
+
+  def create
+    begin
+      post = Post.create(post_params)
+      respond_to do |f|
+        f.html do
+          redirect_to "/posts/#{post.id}"
+        end
       end
     end
   end
@@ -21,4 +37,13 @@ class PostsController < ApplicationController
   def destroy
     if Post.exists?(params[:id])
       Post.destroy(params[:id])
-    render 'destroy.html.erb' #check to change
+      redirect_to "/posts"
+    end
+  end
+
+  def post_blog
+    params.require(:post).permit(:title, :body)
+  end
+end
+
+
